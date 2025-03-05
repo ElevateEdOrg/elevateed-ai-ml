27bffaf16cc57dcc28a21aff6d1eee0989cd94d4
Ubuntu <ubuntu@ip-172-31-28-25.ec2.internal>
2025-03-05 12:16:30 +0000

    Requirements file updated for linux..

--- recommendation/config.py
+++ recommendation/config.py
@@ -3,6 +3,6 @@
 class Config:
     """Configuration for the Flask app and database."""
     # Example:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:300282@192.168.10.49:5432/Elevatedb"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:6YZizt9aFFj2@elevate.cidgqgeogv4w.us-east-1.rds.amazonaws.com:5432/Elevatedb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SECRET_KEY = "some-secret-key"  # Replace with a real secret in production
