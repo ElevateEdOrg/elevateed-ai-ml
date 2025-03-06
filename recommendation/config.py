f3b5051c6b86cfc8341fa98ff489c5bb713afa6b
Dadiya-Harsh-wappnet <harshd.wappnet@gmail.com>
2025-03-06 09:12:19 +0530

    updated connection to remote rds..

--- recommendation/config.py
+++ recommendation/config.py
@@ -1,8 +1,12 @@
 # recommendation/config.py
+import os
 
 class Config:
     """Configuration for the Flask app and database."""
     # Example:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:300282@192.168.10.49:5432/Elevatedb"
+   
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SECRET_KEY = "some-secret-key"  # Replace with a real secret in production
